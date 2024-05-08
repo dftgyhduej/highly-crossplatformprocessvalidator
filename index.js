92140e590f7fff86daea9d0b8198f382ab7cca5e
dftgyhduej <csjcadencochran@hotmail.com>
2024-05-09 03:16:55 +0700

    feat commit

--- index.js
+++ index.js
@@ -1,28 +1,34 @@
-function numIslands(grid) {
-  if (grid.length === 0) return 0;
-  let count = 0;
-  for (let i = 0; i < grid.length; i++) {
-    for (let j = 0; j < grid[0].length; j++) {
-      if (grid[i][j] === "1") {
-        dfs(grid, i, j);
-        count++;
+function solveSudoku(board) {
+  solve(board);
+  function solve(board) {
+    for (let i = 0; i < 9; i++) {
+      for (let j = 0; j < 9; j++) {
+        if (board[i][j] === ".") {
+          for (let num = 1; num <= 9; num++) {
+            const char = num.toString();
+            if (isValid(board, i, j, char)) {
+              board[i][j] = char;
+              if (solve(board)) return true;
+              board[i][j] = ".";
+            }
+          }
+          return false;
+        }
       }
     }
+    return true;
   }
-  return count;
-}
-function dfs(grid, i, j) {
-  if (
-    i < 0 ||
-    i >= grid.length ||
-    j < 0 ||
-    j >= grid[0].length ||
-    grid[i][j] === "0"
-  )
-    return;
-  grid[i][j] = "0";
-  dfs(grid, i + 1, j);
-  dfs(grid, i - 1, j);
-  dfs(grid, i, j + 1);
-  dfs(grid, i, j - 1);
+  function isValid(board, row, col, char) {
+    for (let i = 0; i < 9; i++) {
+      if (
+        board[row][i] === char ||
+        board[i][col] === char ||
+        board[3 * Math.floor(row / 3) + Math.floor(i / 3)][
+          3 * Math.floor(col / 3) + (i % 3)
+        ] === char
+      )
+        return false;
+    }
+    return true;
+  }
 }
