b26d80332f8c54d9203d577c6ee94dbd2f5e49e0
dftgyhduej <csjcadencochran@hotmail.com>
2024-05-11 20:11:06 +0700

    update project

--- index.js
+++ index.js
@@ -1,6 +1,3 @@
-function linearSearch(arr, target) {
-  for (let i = 0; i < arr.length; i++) {
-    if (arr[i] === target) return i;
-  }
-  return -1;
+function isPalindrome(str) {
+  return str === str.split("").reverse().join("");
 }
